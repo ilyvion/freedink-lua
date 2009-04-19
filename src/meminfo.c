13fc14d2b305b4c274f55507e963322c6423109f
Sylvain Beucler <beuc@beuc.net>
2009-04-19 15:03:43 +0200

    Debug GFX/SFX memory usage when loading a screen (2)

---
+++ src/meminfo.c
@@ -0,0 +1,38 @@
+/**
+ * Display malloc stats
+
+ * Copyright (C) 2009  Sylvain Beucler
+
+ * This file is part of GNU FreeDink
+
+ * GNU FreeDink is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+
+ * GNU FreeDink is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <malloc.h>
+
+#include "log.h"
+
+void meminfo_log_mallinfo()
+{
+  struct mallinfo mi;
+  mi = mallinfo();
+  log_debug("arena: %d\n", mi.arena);
+  log_debug("uordblks: %d\n", mi.uordblks);
+  log_debug("fordblks: %d\n", mi.fordblks);
+}
---
+++ src/meminfo.h
@@ -0,0 +1,28 @@
+/**
+ * Memory statistics
+
+ * Copyright (C) 2009  Sylvain Beucler
+
+ * This file is part of GNU FreeDink
+
+ * GNU FreeDink is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+
+ * GNU FreeDink is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _MEMINFO_H
+#define _MEMINFO_H
+
+extern void meminfo_log_mallinfo(void);
+
+#endif
