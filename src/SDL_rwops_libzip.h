fb0057bf38222705322ea08e948423ab5fa886ed
Sylvain Beucler <beuc@beuc.net>
2014-05-04 22:50:24 +0200

    Centralize copyright information some more

--- src/SDL_rwops_libzip.h
+++ src/SDL_rwops_libzip.h
@@ -1,7 +1,26 @@
+/**
+ * libzip data source for SDL_RWops
+
+ * Copyright (C) 2001  Guido Draheim <guidod@gmx.de>
+
+ * This file is part of GNU FreeDink
+
+ * GNU FreeDink is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+
+ * GNU FreeDink is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 /*
- *      Copyright (c) 2001 Guido Draheim <guidod@gmx.de>
- *      Use freely under the restrictions of the ZLIB License
- *
  *      You should be able to drop it in the place of a SDL_RWFromFile. Then
  *      go to X/share/myapp and do `cd graphics && zip -9r ../graphics.zip .`
  *      and rename the graphics/ subfolder - and still all your files
